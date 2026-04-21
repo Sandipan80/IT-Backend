fff66ca50f43b85b092ba2c0e25613f0c44905fe
Sandipan80 <bhullarsandipan@gmail.com>
2026-04-21 10:27:18 +0530

    fixed tickets routes

--- routes/api/Ticket.Route.js
+++ routes/api/Ticket.Route.js
@@ -1,8 +1,11 @@
 const Waiter = require("express").Router()
 const NewTicket = require("../../controllers/create/CreateTicket")
-
+const {getAllTickets, getTicketsByUser } = require("../../controllers/fetch/GetTicket")
 
 Waiter.post("/CreateTicket",NewTicket);
+Waiter.get("/getAllTickets",getAllTickets);
+Waiter.get("/getTicketsByUser/:userId", getTicketsByUser);
+
 
 
 
