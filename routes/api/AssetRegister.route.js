b8d8e2757fcb2c1477b58a5fbf3d50279ff8bc0e
Sandipan80 <bhullarsandipan@gmail.com>
2026-04-22 10:14:18 +0530

    new route for assign asset

--- routes/api/AssetRegister.route.js
+++ routes/api/AssetRegister.route.js
@@ -3,6 +3,7 @@ const Waiter = require("express").Router()
 const addasset = require("../../controllers/create/AddAsset")
 const { GetAllAssets, GetAssetById } = require("../../controllers/fetch/GetAsset")
 const { UpdateAsset, DeleteAsset } = require("../../controllers/create/NewAsset");
+const AssignAsset = require("../../controllers/update/AssignAsset")
 
  
 
@@ -11,5 +12,6 @@ Waiter.get("/GetAllAssets",GetAllAssets)
 Waiter.get("/GetAssetById/:id",GetAssetById)
 Waiter.put("/UpdateAsset/:id",UpdateAsset)
 Waiter.delete("/DeleteAsset/:id",DeleteAsset);
+Waiter.put("/AssignAsset",AssignAsset)
 
 module.exports =Waiter;
