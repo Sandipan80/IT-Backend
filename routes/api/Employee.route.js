752947b4d52925f763da47ca067c5cd49cf6106f
Sandipan80 <bhullarsandipan@gmail.com>
2026-04-22 10:12:25 +0530

    added new routes of update and delete

--- routes/api/Employee.route.js
+++ routes/api/Employee.route.js
@@ -2,10 +2,14 @@ const Waiter = require("express").Router()
 const GetUser = require("../../controllers/fetch/GetEmployee")
 const NewUser = require("../../controllers/create/NewEmployee")
 const Employee_profile = require("../../controllers/fetch/Employee_profile")
+const UpdateEmployee = require("../../controllers/update/UpdateEmployee");
+const DeleteEmployee = require("../../controllers/delete/DeleteEmployee");
 
 
 Waiter.post("/NewUser",NewUser)
 Waiter.get("/GetUser",GetUser)
 Waiter.get("/Employee_profile/:id",Employee_profile)
+Waiter.put("/UpdateUser/:id", UpdateEmployee);
+Waiter.delete("/DeleteUser/:id", DeleteEmployee);
 
 module.exports = Waiter;
