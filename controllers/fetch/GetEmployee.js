f3d1b63b4851ef3e5503e4f78a1d04c61c4f972b
Sandipan80 <bhullarsandipan@gmail.com>
2026-04-22 10:47:34 +0530

    now can fetch details of assets in the Employee table and in assets array

--- controllers/fetch/GetEmployee.js
+++ controllers/fetch/GetEmployee.js
@@ -1,24 +1,22 @@
-const EmployeeSchema = require("../../models/Employee.model")
-const {userValidate}= require("../../validation/User_Validation");
+const EmployeeSchema = require("../../models/Employee.model");
 
-const  GetUser =async(req,res,next)=>{
-//
-try{
-    const registerData = await EmployeeSchema.find();
-  console.log("registerData:", registerData);
-  if (!registerData) {
-    return res.status(400).json({ message: "No Register User !!" });
-  }
-res.status(200).json(
-    {
-        message:"Employee details fetch successfully",
-        employeeList: registerData
+const GetUser = async (req, res, next) => {
+  try {
+    // .populate("Assets") fetches the full details of all assets in the Assets array
+    const registerData = await EmployeeSchema.find().populate("Assets");
+
+    if (!registerData || registerData.length === 0) {
+      return res.status(400).json({ message: "No Registered Users Found!!" });
     }
-);
-}
-catch(error){
-  next(error);
-  console.log("Error in registration:", error);
-}
+
+    res.status(200).json({
+      message: "Employee details fetched successfully",
+      employeeList: registerData,
+    });
+  } catch (error) {
+    console.error("Error in fetching users:", error);
+    next(error);
+  }
 };
+
 module.exports = GetUser;
