6c6cd7afb5acebd65f79f3365cada53e4541f952
Sandipan80 <bhullarsandipan@gmail.com>
2026-04-29 10:53:48 +0530

    new AssetReq Routes

--- routes/api/AssetRegister.route.js
+++ routes/api/AssetRegister.route.js
@@ -4,6 +4,12 @@ const addasset = require("../../controllers/create/AddAsset")
 const { GetAllAssets, GetAssetById } = require("../../controllers/fetch/GetAsset")
 const { UpdateAsset, DeleteAsset } = require("../../controllers/create/NewAsset");
 const AssignAsset = require("../../controllers/update/AssignAsset")
+const createAssetRequest = require("../../controllers/create/AssetRequest")
+const updateAssetRequest = require("../../controllers/update/UpdateAssetRequest")
+const getAllAssetRequests = require("../../controllers/fetch/GetAssetRequest")
+const GetUnassignedAssets = require("../../controllers/fetch/GetUnassignedAssets")
+
+const {verifyToken,isAdmin}= require("../../middleware/auth")
 
  
 
@@ -13,5 +19,12 @@ Waiter.get("/GetAssetById/:id",GetAssetById)
 Waiter.put("/UpdateAsset/:id",UpdateAsset)
 Waiter.delete("/DeleteAsset/:id",DeleteAsset);
 Waiter.put("/AssignAsset",AssignAsset)
+Waiter.post("/createAssetRequest",verifyToken,createAssetRequest)
+Waiter.get("/getAllAssetRequests",verifyToken,isAdmin,getAllAssetRequests)
+Waiter.patch("/updateAssetRequest/:requestID",verifyToken,isAdmin,updateAssetRequest)
+Waiter.get("/GetUnassignedAssets",GetUnassignedAssets)
+
+
+
 
 module.exports =Waiter;
