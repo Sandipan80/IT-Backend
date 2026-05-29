6190cf7914757a2e48dca0bfceac83093412bf96
Sandipan80 <bhullarsandipan@gmail.com>
2026-05-29 14:24:12 +0530

    feat(Routes): add Work route to API index for task management

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,12 +4,14 @@ const LoginRoute  = require("./Login.route")
 const ContactRoute  = require("./Contact.route")
 const EmployeeRoute = require("./Employee.route")
 const TicketRoute = require("./Ticket.Route")
+const WorkRoute = require("./Work.route")
 
 Waiter.use("/Assets",Assets)
 Waiter.use("/LoginRoute",LoginRoute)
 Waiter.use("/ContactRoute",ContactRoute)
 Waiter.use("/EmployeeRoute",EmployeeRoute)
 Waiter.use("/TicketRoute",TicketRoute)
+Waiter.use("/WorkRoute",WorkRoute)
 
 
 
