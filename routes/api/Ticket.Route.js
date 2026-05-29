52b6d46c13891b80d9cd00e9e4e7a2023560fff2
Sandipan80 <bhullarsandipan@gmail.com>
2026-05-29 14:24:27 +0530

    feat(Ticket): add route for assigning tickets to employees

--- routes/api/Ticket.Route.js
+++ routes/api/Ticket.Route.js
@@ -1,10 +1,12 @@
 const Waiter = require("express").Router()
 const NewTicket = require("../../controllers/create/CreateTicket")
 const {getAllTickets, getTicketsByUser } = require("../../controllers/fetch/GetTicket")
+const { assignTicketToEmployee } = require("../../controllers/update/AssignTicket")
 
 Waiter.post("/CreateTicket",NewTicket);
 Waiter.get("/getAllTickets",getAllTickets);
 Waiter.get("/getTicketsByUser/:userId", getTicketsByUser);
+Waiter.put("/assignTicket/:id", assignTicketToEmployee)
 
 
 
